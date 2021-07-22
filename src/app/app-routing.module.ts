dc0fac11e726df3c613efe6935253cd00855349d
alantran99 <tranlevuluan@gmail.com>
2021-07-22 12:29:18 +0700

    pull
    Co-Authored-By: alantran99 <87791935+alantran99@users.noreply.github.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
+import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsComponent } from './modules/forms/forms.component';
+//import { FormComponent } from './modules/form/form.component';
+//import { SidenavComponent } from './modules/sidenav/sidenav.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: '', component: SidenavComponent }
+  { path:'forms', component:FormsComponent},
+  { path: '', redirectTo:'forms', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatSliderModule } from '@angular/material/slider'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatBadgeModule } from '@angular/material/badge'
+import { MatCardModule } from '@angular/material/card'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatInputModule } from '@angular/material/input'
+import { MatDividerModule } from '@angular/material/divider'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './modules/sidenav/sidenav.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { FooterComponent } from './modules/footer/footer.component';
+import { FormsComponent } from './modules/forms/forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatSliderModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatCardModule,
+    MatGridListModule,
+    MatInputModule,
+    MatDividerModule,
+    AppRoutingModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
